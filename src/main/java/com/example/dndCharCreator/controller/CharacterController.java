362241d833bb32375ba975a25b73768bdc878ae2
Vyshnevetskyi Pasha <demonaft1@gmail.com>
2023-07-07 20:27:04 +0300

    add save endpoint

--- src/main/java/com/example/dndCharCreator/controller/CharacterController.java
+++ src/main/java/com/example/dndCharCreator/controller/CharacterController.java
@@ -3,20 +3,17 @@ package com.example.dndCharCreator.controller;
 import com.example.dndCharCreator.entity.Character;
 import com.example.dndCharCreator.service.CharacterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/character")
 public class CharacterController {
 
     private final CharacterService characterService;
 
-    @GetMapping("/{name}")
-    public void save(@PathVariable String name) {
-        Character character = new Character();
-        character.setName(name);
+    @PostMapping()
+    public void save(@RequestBody Character character) {
         characterService.save(character);
     }
 }
