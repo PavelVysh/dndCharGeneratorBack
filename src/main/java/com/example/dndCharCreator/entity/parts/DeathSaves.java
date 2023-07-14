ada13978620f35ea14c072b3edff97ca86684a4b
pvyshnevetskyi <117741996+pvyshnevetskyi@users.noreply.github.com>
2023-07-14 13:46:43 +0300

    Create DeathSaves.java

---
+++ src/main/java/com/example/dndCharCreator/entity/parts/DeathSaves.java
@@ -0,0 +1,10 @@
+package com.example.dndCharCreator.entity.parts;
+
+import lombok.Data;
+
+@Data
+public class DeathSaves {
+    private boolean t1;
+    private boolean t2;
+    private boolean t3;
+}
